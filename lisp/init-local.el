a93f73919f5791a9f39e06d36b5b89b400415b9d
Andrew May <acmay@acmay.org>
2015-11-15 16:35:34 -0800

    My local keymaps and settings

---
+++ lisp/init-local.el
@@ -0,0 +1,61 @@
+(provide 'init-local)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
+
+;;Wipe the entire line instead of just the line contents
+(setq kill-whole-line t)
+
+(require-package 'ggtags)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
+;;; Print out the ascii-table in a new buffer
+(defun ascii-table ()
+  "Print the ascii table. Based on a defun by Alex Schroeder &lt;asc@bsiag.com&gt;"
+  (interactive)
+  (switch-to-buffer "*ASCII*")
+  (erase-buffer)
+  (insert (format "ASCII characters up to number %d.\n" 254))
+  (let ((i 0))
+    (while (< i 254)
+      (setq i (+ i 1))
+      (insert (format "%4d 0x%02x - %c\n" i i i))))
+  (beginning-of-buffer))
+
+
+(global-set-key [C-next] 'scroll-other-window)
+(global-set-key [C-prior] 'scroll-other-window-down)
+
+;;I want top/bottom of the file not the line
+(global-set-key [home] 'beginning-of-buffer)
+(global-set-key [end] 'end-of-buffer)
+
+;;Sometimes this key is not setup but I am not sure why not
+(global-set-key [C-M-s] 'isearch-forward-regexp)
+
+;;I have hit this key too many times
+(global-unset-key "\C-x\C-c")
+(global-unset-key "\C-z")
+(global-unset-key "\C-c\C-c")
+;;(global-set-key "\C-c\C-c" 'compile)
+
+(global-set-key "\C-cg" 'goto-line)
+(global-set-key "%" 'match-paren)
+
+(global-set-key [f1] 'woman)
+(global-set-key [f3] 'rgrep)
+
+(global-set-key [f9] 'recompile)
+
+(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+(global-set-key (kbd "<C-S-right>")  'buf-move-right)
+
+
+;;
+(require-package 'irfc)
+
+(setq irfc-directory "~/Documents/rfc")
+(setq irfc-assoc-mode t)
