e2ec279c1e34fb9362771cc317d45a46e738648e
Andrew May <acmay_code@acmay.org>
2019-07-10 09:36:56 -0700

    Disable Clojure since I don't use it.

--- init.el
+++ init.el
@@ -114,8 +114,8 @@
 (require 'init-paredit)
 (require 'init-lisp)
 (require 'init-slime)
-(require 'init-clojure)
-(require 'init-clojure-cider)
+;;(require 'init-clojure)
+;;(require 'init-clojure-cider)
 (require 'init-common-lisp)
 
 (when *spell-check-support-enabled*
