75c11d73f116a8ad69fdea1eb8c976060192f0b4
Andrew May <acmay_code@acmay.org>
2019-07-10 09:57:19 -0700

    Remove old unworking key bindings

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -59,11 +59,6 @@
 
 (global-set-key [f9] 'recompile)
 
-(global-set-key (kbd "<C-S-up>")     'buf-move-up)
-(global-set-key (kbd "<C-S-down>")   'buf-move-down)
-(global-set-key (kbd "<C-S-left>")   'buf-move-left)
-(global-set-key (kbd "<C-S-right>")  'buf-move-right)
-
 (setq scroll-margin 3)
 
 ;;
